7c29834763c2f3cecfbbd104fba79ccaae946c8a
Viktor Blomme <viktor.blomme@fishingcactus.com>
2021-08-04 16:44:30 +0200

    Refactor GetAllActorsOfClass
    Fix some cs.

--- Source/CoreExtensions/Public/BlueprintLibraries/CoreExtHelperBlueprintLibrary.h
+++ Source/CoreExtensions/Public/BlueprintLibraries/CoreExtHelperBlueprintLibrary.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <CoreMinimal.h>
-#include <Kismet/BlueprintFunctionLibrary.h>
 #include "Engine/Engine.h"
 
+#include <CoreMinimal.h>
 #include <EngineUtils.h>
+#include <Kismet/BlueprintFunctionLibrary.h>
 #include <UObject/SoftObjectPtr.h>
 
 #include "CoreExtHelperBlueprintLibrary.generated.h"
@@ -25,28 +25,13 @@ public:
     UFUNCTION( BlueprintCallable, Category = "Maps" )
     static void OpenMap( const UObject * world_context, const TSoftObjectPtr< UWorld > & map_soft_object_ptr, bool open_if_current = false );
 
+    static bool BrowseMap( FWorldContext & world_context, const TSoftObjectPtr< UWorld > & map_soft_object_ptr, bool open_if_current = false );
+
     template < typename _ACTOR_CLASS_ >
     static _ACTOR_CLASS_ * GetActorOfClass( const UObject * world_context );
 
-    static bool BrowseMap( FWorldContext & world_context, const TSoftObjectPtr< UWorld > & map_soft_object_ptr, bool open_if_current = false );
-
-    template < class T >
-    static FORCEINLINE void GetAllActorsOfClass( const UObject * world_context_object, TSubclassOf< T > actor_class, TArray< T * > & out_actors )
-    {
-        out_actors.Reset();
-
-        if ( actor_class )
-        {
-            if ( auto * world = GEngine->GetWorldFromContextObject( world_context_object, EGetWorldErrorMode::LogAndReturnNull ) )
-            {
-                for ( TActorIterator< T > it( world, actor_class ); it; ++it )
-                {
-                    auto * actor = *it;
-                    out_actors.Add( actor );
-                }
-            }
-        }
-    }
+    template < typename _ACTOR_CLASS_ >
+    static void GetAllActorsOfClass( const UObject * world_context, TArray< _ACTOR_CLASS_ * > & out_actors );
 };
 
 template < typename _ACTOR_CLASS_ >
@@ -54,14 +39,30 @@ _ACTOR_CLASS_ * UCoreExtHelperBlueprintLibrary::GetActorOfClass( const UObject *
 {
     QUICK_SCOPE_CYCLE_COUNTER( UCoreExtHelperBlueprintLibrary_GetActorOfClass );
 
-    if ( UWorld * World = GEngine->GetWorldFromContextObject( world_context, EGetWorldErrorMode::LogAndReturnNull ) )
+    if ( auto * world = GEngine->GetWorldFromContextObject( world_context, EGetWorldErrorMode::LogAndReturnNull ) )
     {
-        for ( TActorIterator< _ACTOR_CLASS_ > actor_iterator( World, _ACTOR_CLASS_::StaticClass() ); actor_iterator; ++actor_iterator )
+        for ( TActorIterator< _ACTOR_CLASS_ > actor_iterator( world, _ACTOR_CLASS_::StaticClass() ); actor_iterator; ++actor_iterator )
         {
-            _ACTOR_CLASS_ * actor = *actor_iterator;
+            auto * actor = *actor_iterator;
             return actor;
         }
     }
 
     return nullptr;
+}
+
+template < typename _ACTOR_CLASS_ >
+void UCoreExtHelperBlueprintLibrary::GetAllActorsOfClass( const UObject * world_context, TArray< _ACTOR_CLASS_ * > & out_actors )
+{
+    QUICK_SCOPE_CYCLE_COUNTER( UCoreExtHelperBlueprintLibrary_GetAllActorsOfClass );
+    out_actors.Reset();
+
+    if ( auto * world = GEngine->GetWorldFromContextObject( world_context, EGetWorldErrorMode::LogAndReturnNull ) )
+    {
+        for ( TActorIterator< _ACTOR_CLASS_ > actor_iterator( world, _ACTOR_CLASS_::StaticClass() ); actor_iterator; ++actor_iterator )
+        {
+            auto * actor = *actor_iterator;
+            out_actors.Add( actor );
+        }
+    }
 }
