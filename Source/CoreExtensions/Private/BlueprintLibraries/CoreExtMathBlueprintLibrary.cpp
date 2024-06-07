fa19212bac3f3ae964c573f3116e9b254688845e
Hades Fury <hadesfury@gmail.com>
2024-06-07 10:34:52 +0200

    [DEV] Generate points on a rectangle edges [Avoid copy with return values]

--- Source/CoreExtensions/Private/BlueprintLibraries/CoreExtMathBlueprintLibrary.cpp
+++ Source/CoreExtensions/Private/BlueprintLibraries/CoreExtMathBlueprintLibrary.cpp
@@ -1,28 +1,24 @@
 #include "BlueprintLibraries/CoreExtMathBlueprintLibrary.h"
 
-TArray< FVector2D > UCoreExtMathBlueprintLibrary::GetPointsOnRectangleEdge( const float height, const float width, const float point_spacing )
+void UCoreExtMathBlueprintLibrary::GetPointsOnRectangleEdges( TArray< FVector2D > & out_point_table, const float height, const float width, const float point_spacing )
 {
-    TArray< FVector2D > result;
-
     float half_width = width / 2.0f;
     float half_height = height / 2.0f;
     int point_count = ceil( ( height + width ) * 2.0f / point_spacing );
 
-    result.Reserve( point_count );
+    out_point_table.Reserve( point_count );
 
     for ( int x = -half_width; x <= half_width; x += point_spacing )
     {
-        result.Add( FVector2D( x, half_height ) );
-        result.Add( FVector2D( x, -half_height ) );
+        out_point_table.Add( FVector2D( x, half_height ) );
+        out_point_table.Add( FVector2D( x, -half_height ) );
     }
 
     for ( int y = -half_height + 1; y <= half_height - 1; y += point_spacing )
     {
-        result.Add( FVector2D( half_width, y ) );
-        result.Add( FVector2D( -half_width, y ) );
+        out_point_table.Add( FVector2D( half_width, y ) );
+        out_point_table.Add( FVector2D( -half_width, y ) );
     }
-
-    return result;
 }
 
 int UCoreExtMathBlueprintLibrary::GetWrappedIndex( const int index, const int size )
