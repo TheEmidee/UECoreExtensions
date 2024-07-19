b0838f9c7b787c403ee1ed8fa81f7f752559f5d5
Thibaut Spreux <thibaut.spreux@fishingcactus.com>
2024-07-19 08:43:13 +0200

    create class and add litview function

---
+++ Source/CoreExtensions/Private/BlueprintLibraries/UCoreExtUIBlueprintLibrary.cpp
@@ -0,0 +1,8 @@
+﻿#include "BlueprintLibraries/UCoreExtUIBlueprintLibrary.h"
+
+#include <Components/ListView.h>
+
+UUserWidget * UUCoreExtUIBlueprintLibrary::GetWidgetFromListViewItem( UListView * list_view, UObject * item )
+{
+    return list_view->GetEntryWidgetFromItem( item );
+}
